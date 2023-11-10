8139db9aee3b3db8987715b364151c0b38a9f9b4
Poserko1 <124555732+Poserko1@users.noreply.github.com>
2023-11-10 11:34:20 +0200

    Add files via upload

---
+++ DogsApp/DogsApp/Controllers/DogController.cs
@@ -0,0 +1,111 @@
+﻿using DogsApp.Infrastructure.Data;
+using DogsApp.Infrastructure.Data.Domain;
+using DogsApp.Models.Dog;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogsApp.Controllers
+{
+    public class DogController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+            public DogController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: Dogs
+        public ActionResult Index()
+        {
+            List<DogAllViewModel> dogs = _context.Dogs.Select(x => new DogAllViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Age = x.Age,
+                Breed = x.Breed,
+                Pictures = x.Picture
+            }).ToList();
+            return View(dogs);
+        }
+
+        // GET: Dogs/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Dogs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dogs/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(DogCreateViewModel bindingModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Dog dogFromDb = new Dog
+                {
+                    Name = bindingModel.Name,
+                    Age = bindingModel.Age,
+                    Breed = bindingModel.Breed,
+                    Picture = bindingModel.Pictures
+
+                };
+                _context.Dogs.Add(dogFromDb);
+                _context.SaveChanges();
+
+                return RedirectToAction("Success");
+            }
+            return View();
+        }
+        public IActionResult Success()
+        {
+            return View();
+        }
+        // GET: Dogs/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Dogs/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Dogs/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Dogs/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DogsApp/DogsApp/Models/Dog/DogAllViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace DogsApp.Models.Dog
+{
+    public class DogAllViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public int Age { get; set; }
+
+
+        public string Breed { get; set; } = null!;
+
+        public string? Pictures { get; set; }
+    }
+}
